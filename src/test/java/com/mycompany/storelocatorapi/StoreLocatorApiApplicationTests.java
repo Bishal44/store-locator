773dc9040bc409d86977c46f490498fea537defc
bishal <bhattaraibishal704@gmail.com>
2022-07-10 14:59:59 +0200

    init

---
+++ src/test/java/com/mycompany/storelocatorapi/StoreLocatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.storelocatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreLocatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
