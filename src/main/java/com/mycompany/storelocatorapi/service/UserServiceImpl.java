08138fa637ccc75b22da4bbdbb18e819153774e0
bishal <bhattaraibishal704@gmail.com>
2022-07-20 22:40:18 +0200

    Service Layer only login

---
+++ src/main/java/com/mycompany/storelocatorapi/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.mycompany.storelocatorapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public void login() {
+
+    }
+}
